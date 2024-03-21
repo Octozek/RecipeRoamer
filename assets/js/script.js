9f5e646755164fe4b3dd1c186df06f85a632fcaf
BiggieLittles <156357409+BiggieLittles@users.noreply.github.com>
2024-03-21 11:37:28 -0400

    functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,45 +31,113 @@ var secondModal = document.getElementById("secondModal");
 var saveChangesBtn = document.querySelector(".modal-card-foot .is-success");
 
 // When the user clicks the "Save changes" button, open the second modal and close the current modal
-saveChangesBtn.onclick = function() {
-    modal.classList.remove("is-active"); // Close the current modal
-    secondModal.classList.add("is-active"); // Open the second modal
-    requestAPI()
-  }
+saveChangesBtn.onclick = function () {
 
-// When the user clicks on the close button of the second modal, close the second modal
-document.querySelector("#secondModal .delete").onclick = function() {
-  secondModal.classList.remove("is-active");
-}
+  // saves the information from the user's search
+  var keywordInput = document.querySelector("#search-input").value;
+  var dishTypeInput = document.querySelector("#dish-type-input").value;
+  var mealTypeInput = document.querySelector("#meal-type-input").value;
+  var cuisineTypeInput = document.querySelector("#cuisine-type-input").value;
 
-// ---------- API Request Below -----------
+  console.log(keywordInput);
+  console.log(dishTypeInput);
+  console.log(mealTypeInput);
+  console.log(cuisineTypeInput);
 
+  modal.classList.remove("is-active"); // Close the current modal
 
+  secondModal.classList.add("is-active"); // Open the second modal
 
+  // Function for calling the API
+  function requestAPI() {
+    // Defines utility variables for API fetch
+    var APIKey = "&app_key=46ed35f132f42ee1119d14f95de261df";
+    var APIId = "&app_id=3577f865";
+    var APIBaseURL = "https://api.edamam.com/api/recipes/v2?type=public";
+    // Defines usable query/parameters for API fetch using user's search criteria
+    var keyword = "&q=" + keywordInput;
+    var dishType = "&dishType=" + dishTypeInput;
+    var mealType = "&mealType=" + mealTypeInput;
+    var cuisineType = "&cuisineType=" + cuisineTypeInput;
+    // Checks to see if any of the parameters are blank. If so, removes them
+    if (keywordInput = "") {
+      keyword = "";
+    }
+    if (dishTypeInput = "Select dropdown") {
+      dishType = "";
+    }
+    if (mealTypeInput = "Select dropdown") {
+      mealType = "";
+    }
+    if (cuisineTypeInput = "Select dropdown") {
+      cuisineType = "";
+    }
 
-function requestAPI() {
-  var APIKey = "&app_key=46ed35f132f42ee1119d14f95de261df";
-  var APIId = "&app_id=3577f865";
-  var APIBaseURL = "https://api.edamam.com/api/recipes/v2?type=public";
-
-  var keywordInput = document.getElementById("searchInput").value.trim();
-  var cuisineTypeInput = "Asian";
-  var mealTypeInput = "Dinner";
-
-  var keyword = "&q=" + keywordInput;
-  var cuisineType = "&cuisineType=" + cuisineTypeInput;
-  var mealType = "&mealType=" + mealTypeInput;
-
-  fetch(APIBaseURL + keyword + APIId + APIKey + cuisineType + mealType)
-    .then(function (response) {
-      return response.json();
-    })
+    fetch(APIBaseURL + keyword + APIId + APIKey + cuisineType + mealType + dishType)
+      .then(function (response) {
+        return response.json();
+      })
       .then(function (data) {
-        // Display random recipes in the text input
+        console.log(data);
         if (data.hits.length > 0) {
           var randomIndex = Math.floor(Math.random() * data.hits.length);
           var randomRecipeLabel = data.hits[randomIndex].recipe.label;
-          document.getElementById("searchInput").value = randomRecipeLabel;
+          document.getElementById("search-input").value = randomRecipeLabel;
+          // Create container to hold recipe cards
+          var recipeCards = document.createElement("div")
+          recipeCards.classList.add("recipeCards")
+
+          // Add the cards to the div
+          var cardDisplay = document.querySelector(".section .container")
+          cardDisplay.appendChild(recipeCards)
+
+          // Use API and go through each "hit"
+          data.hits.forEach(function (hit) {
+            var recipe = hit.recipe;
+
+            // Create card
+            var recipeCard = document.createElement("div");
+            recipeCard.classList.add("box", "has-text-centered");
+
+            //use bulma title styling to add title to each card
+            var recipeTitle = document.createElement("h2");
+            recipeTitle.textContent = recipe.label;
+            recipeTitle.classList.add("card-title", "is-4", "m-2");
+
+            // Add image
+            var recipeImage = document.createElement("img");
+            recipeImage.src = recipe.image;
+
+            // Add link to recipe
+            var recipeLink = document.createElement("a");
+            recipeLink.textContent = "Link to Recipe";
+            recipeLink.classList.add("button", "is-fullwidth", "is-link", "is-outlined", "m-2");
+            recipeLink.setAttribute("href", recipe.url);
+
+            // Add button to link to Google Maps
+            var mapsLink = document.createElement("button");
+            mapsLink.textContent = "Search Google Maps for restaurants";
+            mapsLink.classList.add("button", "is-fullwidth", "is-success", "is-outlined", "m-2");
+
+            // Add button for user to save to favorites
+            var saveRecipe = document.createElement("button");
+            saveRecipe.textContent = "Add Recipe to Favorites";
+            saveRecipe.classList.add("button", "is-fullwidth", "is-danger", "is-outlined", "m-2");
+            saveRecipe.classList.add("data-url", recipe.url);
+
+            // Add image and title
+            recipeCard.appendChild(recipeTitle);
+            recipeCard.appendChild(recipeImage);
+            recipeCard.appendChild(recipeLink);
+            recipeCard.appendChild(mapsLink);
+            recipeCard.appendChild(saveRecipe);
+
+
+            // Add the card to the cards div
+            recipeCards.appendChild(recipeCard);
+          });
+
+
         } else {
           document.getElementById("searchInput").value = "No recipes found";
         }
@@ -78,39 +146,10 @@ function requestAPI() {
         console.error("There was a problem with the fetch operation:", error);
       });
   };
+  requestAPI();
+}
 
-      // Create container to hold recipe cards
-      var recipeCards = document.createElement("div")
-      recipeCards.classList.add("recipeCards")
-
-      // Add the cards to the div
-      var cardDisplay = document.querySelector(".section .container")
-      cardDisplay.appendChild(recipeCards)
-
-      // Use API and go through each "hit"
-      data.hits.forEach(function(hit){
-        var recipe = hit.recipe;
-
-        // Create card
-        var recipeCard = document.createElement("div")
-        recipeCard.classList.add("box")
-
-        //use bulma title styling to add title to each card
-        var recipeTitle = document.createElement("h2");
-        recipeTitle.textContent = recipe.label;
-        recipeTitle.classList.add("title", "is-4");
-
-        // Add image
-        var recipeImage = document.createElement("img")
-        recipeImage.src = recipe.image
-
-        // Add image and title
-        recipeCard.appendChild(recipeTitle);
-        recipeCard.appendChild(recipeImage)
-
-        // Add the card to the cards div
-        recipeCards.appendChild(recipeCard)
-      })
-    ;
-
-; 
+// When the user clicks on the close button of the second modal, close the second modal
+document.querySelector("#secondModal .delete").onclick = function () {
+  secondModal.classList.remove("is-active");
+}
